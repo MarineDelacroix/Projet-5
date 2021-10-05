e022a721ecedf2362c94706b1ea7abb4c51607f7
Marine Delacroix <m.delcrx@gmail.com>
2021-10-05 18:12:56 +0200

    Replacement de fichiers JS

--- back/JS/index.js
+++
@@ -1,7 +0,0 @@
-const url="http://localhost:3000/api/products"
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data[0].description);
-    })
-    .catch((erreur) => console.log("erreur : " + erreur));
