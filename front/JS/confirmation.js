eb4d31ca88dfc90cff602d647210e5173b7df137
Marine Delacroix <m.delcrx@gmail.com>
2021-11-19 12:37:55 +0100

    Finalisation du document confirmation.js

--- front/JS/confirmation.js
+++ front/JS/confirmation.js
@@ -1,9 +1,10 @@
-
+// récupération des informations de commande 
 const orderId = JSON.parse(localStorage.getItem("orderId"));
 const order = JSON.parse(localStorage.getItem("order"));   
-console.log(order);             
+// affichage dans la console des informations de commande (contenant contact products et l'orderId)
+console.log(order);      
+// récupération dans le DOM de la partie HTML à modifier pour afficher le numéro de commande.        
 const orderNumber = document.getElementById("orderId");
 orderNumber.innerHTML += `${orderId}`;
-// Une fois le message affiché, le localStorage est vidé de ses données. 
-               
+// Une fois le message affiché, le localStorage est vidé de ses données.                
 localStorage.clear();
